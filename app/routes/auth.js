512fe39480e289600eed0889db7851878ac9c650
Customer_Support_1 <daniellejgilmore@gmail.com>
2018-06-30 11:52:46 -0700

    trying to fix the signout

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -20,7 +20,7 @@ app.post('/signup', passport.authenticate('local-signup',  { successRedirect: '/
 app.get('/dashboard',isLoggedIn, authController.dashboard);
 
 
-app.get('/logout',authController.logout);
+app.get('/signout',authController.logout);
 
 
 app.post('/signin', passport.authenticate('local-signin',  { successRedirect: '/dashboard', failureRedirect: '/signinERROR'}));
