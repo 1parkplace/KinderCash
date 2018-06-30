ae3af571243802a80f1fe070e43cebc4f5eb228d
Customer_Support_1 <daniellejgilmore@gmail.com>
2018-06-30 12:10:35 -0700

    attempting to get the logout with passport working

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -20,9 +20,7 @@ app.post('/signup', passport.authenticate('local-signup',  { successRedirect: '/
 app.get('/dashboard',isLoggedIn, authController.dashboard);
 
 
-app.get('/signout',authController.logout);
-
-
+app.get('/logout',authController.logout);
 
 
 app.post('/signin', passport.authenticate('local-signin',  { successRedirect: '/dashboard', failureRedirect: '/signinERROR'}));
