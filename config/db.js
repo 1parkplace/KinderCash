0e31521f641bb4ce6387b41968590bded70e28ec
Customer_Support_1 <daniellejgilmore@gmail.com>
2018-07-19 18:25:40 -0700

    stuff

---
+++ config/db.js
@@ -0,0 +1,21 @@
+module.exports = {
+    'connection': {
+       // port     : 3306,
+       // host     : 'localhost',
+       // user     : 'root',
+       // password : 'root',
+       // database : 'kinderbeta',
+       // multipleStatements: true
+       port        : 3306,
+       host        : 'g9fej9rujq0yt0cd.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+       user        : 'irc3t3lmssm6sidm',
+       password    : 'dmcfapm71wdjjg5p',
+       database    : 'i15fezoxeewxlrkw',
+       multipleStatements: true
+
+    },
+    //'database': 'kinderbeta',
+    //'users_table': 'users'
+    'database': 'i15fezoxeewxlrkw',
+    'users_table': 'users'
+};
