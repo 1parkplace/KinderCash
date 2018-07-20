913bb60792524302e0550fa80f76c2300d6b7018
Customer_Support_1 <daniellejgilmore@gmail.com>
2018-07-19 18:27:01 -0700

    stuff

--- config/passport/db.js
+++ config/passport/db.js
@@ -1,13 +1,11 @@
 module.exports = {
  
     "development": {
-        "port"    : 3306,
-        "username": "root",
-        "password": "root",
-        "database": "kindercash",
-        "host": "locatlhost",
-        "dialect": "mysql",
-        multipleStatements: true
+        "username": "sq6vji29as4zey8a",
+        "password": "zkxonv4c7hf9i0zo",
+        "database": "gzpd7dwapcp646gx",
+        "host": "o677vxfi8ok6exrd.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        "dialect": "mysql"
     },
    
     "test": {
